1c5bf8966b582e9e9488d126b3785f65a958b139
joaopedroRodriguel <rodrigues.sousa@academico.ifpb.edu.br>
2023-05-01 13:37:38 -0300

    pequenas alteracoes

--- src/main/java/br/edu/ifpb/pweb2/emissordec/service/PeriodoLetivoService.java
+++ src/main/java/br/edu/ifpb/pweb2/emissordec/service/PeriodoLetivoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +24,7 @@ public class PeriodoLetivoService {
     public Optional<PeriodoLetivo> search(Long id) {
         return periodoLetivoRepository.findById(id);
     }
-
+    @Transactional
     public PeriodoLetivo insert(PeriodoLetivo periodoLetivo){
         return periodoLetivoRepository.save(periodoLetivo);
     }
